cd7d3740319d16de4b89195de5174a563963d5a2
Hector Sanjuan <hector@convivencial.org>
2013-02-04 23:48:52 +0100

    Bump to 0.1.11

--- controllers/yeapsy-server.rb
+++ controllers/yeapsy-server.rb
@@ -20,7 +20,7 @@
 #                                                                             #
 ###############################################################################
 
-YEAPSY_VERSION = "0.1.10"
+YEAPSY_VERSION = "0.1.11"
 
 BASE_DIR = File.join(File.dirname(__FILE__), '..')
 CHANGELOG = File.join(BASE_DIR, 'CHANGELOG')
